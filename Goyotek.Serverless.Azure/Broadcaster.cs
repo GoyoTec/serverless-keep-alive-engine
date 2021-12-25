5099cefc40e2d6ca174cb489558932d64b1c2c0d
Judas Echeverria <judas1987@gmail.com>
2021-12-24 20:23:20 -0700

    Initialized all project I think will be required

---
+++ Goyotek.Serverless.Azure/Broadcaster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Goyotek.Serverless.Azure
+{
+    public static class Broadcaster
+    {
+        [FunctionName("Broadcaster")]
+        public static void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
